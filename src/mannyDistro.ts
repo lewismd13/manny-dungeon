b4b8ed2aba199bdaa721311921de040a255fb053
lewismd13 <72758390+lewismd13@users.noreply.github.com>
2021-10-03 17:31:53 -0500

    cleaning up old unused code

--- src/mannyDistro.ts
+++ src/mannyDistro.ts
@@ -1,21 +1,17 @@
 import { myName, print, visitUrl } from "kolmafia";
 import { $item, Kmail } from "libram";
 import { getHoboRunners } from "./lib";
-// import { uniq } from "lodash";
 
 const getTurns = /\((\d+) turns?\)/m;
 const getName = /([\w\s_]+) \(#(\d+)\)/m;
-// const getName2 = /([\w\s_]+) \(#(\d+)\)/gm;
 const page = visitUrl("clan_raidlogs.php");
 const hoboStart = page.indexOf("<div id='Hobopolis'>");
 const hoboLog = page.slice(hoboStart + 52, page.indexOf("<p><b>Loot Distribution:</b>", hoboStart));
 const l = page.indexOf("<b>Sewers:</b><blockquote>");
 const sewerLog = page.slice(l, page.indexOf("</blockquote>", l));
 const nonSewerLog = hoboLog.replace(sewerLog, "");
-// const playerNames = hoboLog.match(getName2);
-// const uniq = [...new Set(playerNames)];
 const playerTable = getHoboRunners(page);
-// TODO: make this part less stupid. somehow need to tie variable to $item. maybe another map
+// TODO: make this part less stupid. probably needs to be configurable. or parse the BKer kmail, which seems hard.
 const forks = 124;
 const mugs = 127;
 const juice = 132;
@@ -24,8 +20,9 @@ const sliders = 130;
 const stews = 0;
 const blankets = 0;
 const banquets = 5;
-
 const dungeonCost = 2350000;
+const cagebaitPlayers = "cagepole sitta,bombastus,stupidsexyflanders,ricardos3";
+const bosskillers = "phillanthropist";
 
 const consumables = new Map([
   [$item`Ol' Scratch's salad fork`, forks],
@@ -37,9 +34,6 @@ const consumables = new Map([
   [$item`tin cup of mulligan stew`, stews],
   [$item`Hodgman's blanket`, blankets],
 ]);
-
-const cagebaitPlayers = "cagepole sitta,bombastus,stupidsexyflanders,ricardos3";
-const bosskillers = "phillanthropist";
 
 // TODO: do this the right way
 // TODO: make names lowercase everywhere
@@ -83,38 +77,9 @@ for (const element of lines) {
       const tt1 = parseInt(t ? t[1] : "");
       print(`${nn1} spent ${tt1} useful turns in the sewers`);
 
-      /*
-      const n = element.match(getName);
-      // there's probably a way to get just the name in regexp
-      // but I haven't figured it out, so we're isolating it here
-      let n1;
-      if (n) {
-        n1 = n.toString();
-      }
-      n1 = n1?.slice(0, n1.indexOf(` (#`));
-*/
-      /*
-      // now grab turns from string
-      const t = element.match(getTurns);
-      // again, there's probably a better way, but now isolating just the number
-      let t1;
-      if (t) {
-        t1 = t.toString();
-      } */
-
-      // t1 = t1?.slice(1, t1.indexOf(` turn`));
-      // t1 = t1?.toString();
-      // find the player for this line, add the turns to their record in the playerTable map
-
-      // TODO: this part needs to to actually use tt[1] and nn[1]
-
-      // const tt1 = tt[1].toString();
-      // const nn1 = nn[1].toString();
-      // print(`adding ${tt1} turns to ${nn1}`);
-
       // grab the current number of turns assigned to the player
       let addturns = playerTable.get(nn1);
-      // print(`${addturns}`);
+
       // add the useful sewer turns to player's total turns and put that number in the map
       addturns += tt1;
       print(`${addturns}`);
@@ -137,16 +102,18 @@ for (const n of lines2) {
     if (!n.includes("defeated by") && !n.includes("went shopping") && n.includes("turn")) {
       // first we extract the player name from the line
       const name = getName.exec(n);
+
       // now make sure we have just the name and that it's a string
       const justName = name ? name[1] : "";
 
       // now we extract the number of turns spent
       const turns = getTurns.exec(n);
+
       // again get just the number and make sure it's a number
       const justTurns = turns ? turns[1] : "";
+
       // now we use add the turns to that player's entry in the player table
       if (justTurns) {
-        // justTurns = justTurns.slice(1, justTurns.indexOf(` turn`));
         let addturns = playerTable.get(justName);
         addturns += parseInt(justTurns);
         playerTable.set(justName, addturns);
@@ -163,65 +130,6 @@ for (const key of playerTable.keys()) {
   print(`${foo}`);
 }
 
-/*
-for (const n of lines2) {
-  if (n) {
-    if (!n.includes("defeated by") && !n.includes("went shopping") && n.includes("turn")) {
-      // first we extract the player name from the line
-      // print(`${n}`);
-
-      const name = n.match(getName);
-      // print(`${name}`);
-      let justName;
-      if (name) {
-        justName = name.toString();
-      }
-      if (justName) {
-        justName = justName.slice(0, justName?.indexOf(` (#`));
-      }
-      // now we extract the number of turns spent
-      const turns = n.match(getTurns);
-      let justTurns;
-      if (turns) {
-        justTurns = turns.toString();
-      }
-      if (justTurns) {
-        justTurns = justTurns.slice(1, justTurns.indexOf(` turn`));
-        let addturns = playerTable.get(justName);
-        addturns += parseInt(justTurns);
-        playerTable.set(justName, addturns);
-      }
-    }
-  }
-}
-*/
-/*
-const totalTurns = sewerLog.match(getTurns);
-let sewerTurns = 0;
-
-totalTurns?.forEach((element) => {
-  print(element);
-  const n = element.slice(1, element.indexOf(` `));
-  print(n);
-  sewerTurns += parseInt(n);
-  print(`${sewerTurns}`);
-});
-
-const nonSewerTurns = nonSewerLog.match(getTurns);
-let otherTurns = 0;
-
-if (nonSewerTurns) {
-  nonSewerTurns.forEach((turn) => {
-    print(turn);
-    const n = turn.slice(1, turn.indexOf(` `));
-    otherTurns += parseInt(n);
-    print(`${otherTurns}`);
-  });
-}
-
-// print(`${sewerTurns} total turns spent in sewer`);
-print(`${otherTurns} total turns spent in non-sewer hobopolis`);
-*/
 let totalUseful = 0;
 playerTable.forEach((value, key) => {
   const n = playerTable.get(key);
@@ -260,14 +168,5 @@ for (const player of playerTable.keys()) {
       }
 */
     }
-
-    // const s = Math.round(sliders * p);
-
-    // loot.set($item`extra-greasy slider`, `${s}`);
-
-    // print(`${player} gets ${s} sliders`);
-    // if (player.toLowerCase() !== myName().toLowerCase()) {
-    // Kmail.send(`${player}`, `you owe ${meatOwed}`, loot);
-    // }
   }
 }
