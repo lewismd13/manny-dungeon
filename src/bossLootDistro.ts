dfaf57075ce049bbe0177c4ee3c3df1425829841
lewismd13 <72758390+lewismd13@users.noreply.github.com>
2021-10-08 16:58:24 -0500

    fix total turns calc, add TODOs

--- src/bossLootDistro.ts
+++ src/bossLootDistro.ts
@@ -1,6 +1,8 @@
 import { bossLootDistro, raidlog } from "./lib";
 import { getHoboRunners, getNonSewerTurns, getSewerLog, getSewerTurns } from "./parsers";
 
+// TODO: have sim and live modes
+
 let playerTable = getHoboRunners(raidlog);
 
 const sewerLog = getSewerLog(raidlog);
