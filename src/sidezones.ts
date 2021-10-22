0baf180c3fb73bb080e9bf6828859952debc4ca5
lewismd13 <72758390+lewismd13@users.noreply.github.com>
2021-10-22 16:39:49 -0500

    turns per player, more zones

--- src/sidezones.ts
+++ src/sidezones.ts
@@ -7,6 +7,29 @@ const eelog = stringLog.match(/<b>Exposure Esplanade:(.*?)<\/blockquote>/);
 const esplanadeLog = eelog ? eelog[1].toString() : "";
 const bblog = stringLog.match(/<b>Burnbarrel Blvd.:(.*?)<\/blockquote>/);
 const burnbarrelLog = bblog ? bblog[1].toString() : "";
+const heap = stringLog.match(/<b>The Heap:(.*?)<\/blockquote>/);
+const heapLog = heap ? heap[1].toString() : "";
+const pld = stringLog.match(/<b>The Purple Light District:(.*?)<\/blockquote>/);
+const pldLog = pld ? pld[1].toString() : "";
+const ahbg = stringLog.match(/<b>The Ancient Hobo Burial Ground:(.*?)<\/blockquote>/);
+const ahbgLog = ahbg ? ahbg[1].toString() : "";
+
+/*
+/Alium \(#\d+\)(?:\s|\w)+(?:pipes?) (?:\((\d+) turns?\))/gm
+*/
+export function getPlayerTurns(clanLog: string, name: string, action: string): number {
+  const getTurns = RegExp(
+    `${name} \\(#\\d+\\)(?:\\s|\\w)+(?:${action}).+?(?:\\((\\d+) turns?\\))`,
+    `gmi`
+  );
+  let turns;
+  let totalTurns = 0;
+  while ((turns = getTurns.exec(clanLog)) !== null) {
+    // print(`${turns[1]} turns`);
+    totalTurns += parseInt(turns[1]);
+  }
+  return totalTurns;
+}
 
 // generic regexp that gets total turns of an action in the dungeon
 function getActionTurns(clanLog: string, action: string) {
@@ -62,13 +85,33 @@ export class Burnbarrel {
   static turns = getTotalTurns(this.log);
   static playerTable = getHoboRunners(this.log);
 
-  static totalStacked(): number {
-    return getActionTurns(this.log, "on the fire");
+  static tiresStacked(name?: string): number {
+    if (!name) return getActionTurns(this.log, "on the fire");
+    else return getPlayerTurns(this.log, name, "on the fire");
   }
-  static totalvalanches(): number {
-    return getActionTurns(this.log, "tirevalanche");
+  static tirevalanches(name?: string): number {
+    if (!name) return getActionTurns(this.log, "tirevalanche");
+    else return getPlayerTurns(this.log, name, "tirevalanche");
   }
   static totalDefeats(): number {
     return getActionTurns(this.log, "defeated by");
   }
+}
+
+export class Heap {
+  static log = heapLog;
+  static turns = getTotalTurns(this.log);
+  static playerTable = getHoboRunners(this.log);
+}
+
+export class PLDistrict {
+  static log = pldLog;
+  static turns = getTotalTurns(this.log);
+  static playerTable = getHoboRunners(this.log);
+}
+
+export class BurialGround {
+  static log = ahbgLog;
+  static turns = getTotalTurns(this.log);
+  static playerTable = getHoboRunners(this.log);
 }
